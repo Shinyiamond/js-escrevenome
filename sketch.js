2312ff7dcc5ce0fa32810f512ad676d28ddb26ce
Shinyiamond <a.icaro@escola.pr.gov.br>
2024-09-11 11:40:46 -0300

    sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,14 @@
+// fantasia, terror, comédia
+
+// Sonic 3, Livre, fantasia, comédia
+// Era do Gelo 1,Livre, fantasia, comédia
+// Jurassic Park, 12, fantasia, terror
+// Exorcismo, 16, terror, fantasia
+// Thor Ragnarok, 12, comédia, fantasia
+// Um Filme Minecraft, Livre, comédia, fantasia
+
+idade >= 10
+
 function setup() {
     createCanvas(400, 400);
   }
