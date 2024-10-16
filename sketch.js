64a1f4e63b2a016d201a58d48179e936b7063414
Shinyiamond <a.icaro@escola.pr.gov.br>
2024-10-16 11:41:12 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,12 +7,53 @@
 // Thor Ragnarok, 12, comédia, fantasia
 // Um Filme Minecraft, Livre, comédia, fantasia
 
-idade >= 10
+let campoIdade;
+let campoFantasia;
+let campoAventura;
 
 function setup() {
     createCanvas(400, 400);
-  }
+    createElement("h2", "Recomendador de filmes");
+  createSpan("Sua idade:");
+  campoIdade = createInput("5");
+  campoFantasia = createCheckbox("Gosta de fantasia?");
+  campoAventura = createCheckbox("Gosta de aventura?");
+}
   
   function draw() {
-    background(220);
-  }
+   background("white");
+  let idade = campoIdade.value();
+  let gostaDeFantasia = campoFantasia.checked();
+  let gostaDeAventura = campoAventura.checked();
+  let recomendacao = geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura);
+
+fill(color(76, 0, 115));
+  textAlign(CENTER, CENTER);
+  textSize(38);
+  text(recomendacao, width / 2, height / 2);
+}
+function geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura) {
+  if (idade >= 10) {
+    if (idade >= 16) {
+      return "O menino que descobriu o vento";
+    } else {
+      if (idade <= 12) {
+        if(gostaDeFantasia || gostaDeAventura) {
+          return "Thor Ragnarok";          
+        } else{
+         return "Jurassic Park";
+        }
+      } else {
+        if (gostaDeFantasia) {
+          return "Era do Gelo 1";
+        } else {
+          return "Sonic 3";
+        }
+      }
+    }
+  } else {
+    if (gostaDeFantasia) {
+      return "Um Filme Minecraft";
+    }
+      if (idade = 16) {
+          return " Exorxismo "
